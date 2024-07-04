d02c08288b657f8fc0d2a501f656e86953cec7ad
thebuttler <netoarevalo@gmail.com>
2024-07-04 03:22:10 -0500

    Comment for clarification

--- server/user/fetch_balance.py
+++ server/user/fetch_balance.py
@@ -1,5 +1,6 @@
 from web3 import Web3
 
+# temporary bad use of url this should be stored in a environment variable
 def get_eth_balance(eth_address):
     infura_url = "https://mainnet.infura.io/v3/c1dfef79061946b28035cda7c0545aab"
     web3 = Web3(Web3.HTTPProvider(infura_url))
